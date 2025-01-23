bb266dc323deaf985914b1da2256aaf9e70d9089
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-01-23 09:39:36 +0000

    [Add]updateError

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,16 @@ class BooksController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book = update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
