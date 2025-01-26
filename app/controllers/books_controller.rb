930f860c7b4c146105005331ea68477bf7b96116
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-01-26 23:21:23 +0000

    [Add]create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
