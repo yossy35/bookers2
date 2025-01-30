34872ee0d535fbb7e519fe9db7fbeb00b0e682f5
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-01-30 09:42:56 +0000

    [revised]UpdateBook_Show_Back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "successfully"
-      redirect_to user_path(user.id)
+      redirect_to user_path(@user.id)
     else
       flash.now[:notice] = "error"
       render :edit
